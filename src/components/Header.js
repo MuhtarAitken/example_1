7762a29496e1c9b519f0e1eb83b0a44929100e73
Dulat <d.bukenbayev@statdata.kz>
2021-03-20 20:26:07 +0600

    add react components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+       <header className='header'><img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT-_mIfmXfxZydIr5sSyiVu6p_LMiG6L8ZDw&usqp=CAU"/>
+    </header>
+);
+}
+export default Header;
